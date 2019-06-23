12a12948b5cb5120203c71d868ef05a9b2a74645
J Lobos Vega <jalv.vg@gmail.com>
2019-06-22 20:48:40 -0400

    friend.js created
    added friends arrrays and server.js

---
+++ app/data/friends.js
@@ -0,0 +1,103 @@
+
+var friends = [
+    {
+      name: "Hilary Bloop",
+      photo: "../public/images-friends/female20171026027991424.jpg",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Carson FLute",
+      photo: "../public/images-friends/male1085051430512.jpg",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Nancy Stern",
+      photo: "../public/images-friends/male20151086268351349.jpg",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Delia Vega",
+      photo: "../public/images-friends/male20151083598842529.jpg",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "Michelle Cepero",
+      photo: "../public/images-friends/May-15-2019 11-00-44.gif",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Paul Douche",
+      photo: "../public/images-friends/tenor-262596434.gif",
+      scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    }
+  
+  ];
+  
+  // Here we export the array. This makes it accessible to other files using require.
+  module.exports = friends;
